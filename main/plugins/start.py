e32b83a36140a27e17e7cba3c8cdcb55ceef3eba
shakil1723 <88028211+shakil1723@users.noreply.github.com>
2023-01-31 19:09:44 +0600

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -4,7 +4,7 @@ import os
 from .. import bot as Drone
 from telethon import events, Button
 
-#from ethon.mystarts import start_srb
+from ethon.mystarts import start_srb
     
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
@@ -45,7 +45,6 @@ async def remt(event):
 @Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     text = "Send me Link of any message to clone it here, For private channel message, send invite link first, Do Not Spam."
-#    await start_srb(event, text)
-    await event.reply(st, 
-                      buttons=[Button.url("DEV", url="t.me/MaheshChauhan")])
+    await start_srb(event, text)
+
     
