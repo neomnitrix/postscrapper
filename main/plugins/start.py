68b740bc736ca88e026706ef6393a09a616a0896
shakil1723 <88028211+shakil1723@users.noreply.github.com>
2023-02-01 09:49:48 +0600

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -3,10 +3,10 @@
 import os
 from .. import bot as Drone
 from telethon import events, Button
-
-from ethon.mystarts import start_srb
+# changes
+#from ethon.mystarts import start_srb
     
-S = '/' + 's' + 't' + 'a' + 'r' + 't'
+#S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
 @Drone.on(events.callbackquery.CallbackQuery(data="set"))
 async def sett(event):    
@@ -42,9 +42,9 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
+"""@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
     text = "Send me Link of any message to clone it here, For private channel message, send invite link first, Do Not Spam."
-    await start_srb(event, text)
+    await start_srb(event, text)"""
 
     
