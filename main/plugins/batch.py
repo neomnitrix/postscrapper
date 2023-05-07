112ebe01ecfe450dca4d69f33a0a017a41826c96
neomnitrix <127597790+neomnitrix@users.noreply.github.com>
2023-05-07 19:28:40 +0530

    Update batch.py

--- main/plugins/batch.py
+++ main/plugins/batch.py
@@ -22,16 +22,15 @@ from pyrogram.errors import FloodWait
 from ethon.pyfunc import video_metadata
 from ethon.telefunc import force_sub
 
-ft = f"To use this bot you've to join @{fs}.<br/> Do Not Spam"
+ft = f"To use this bot you've to join @{fs}."
 
 batch = []
-batch_ = []
 
 async def get_pvt_content(event, chat, id):
     msg = await userbot.get_messages(chat, ids=id)
     await event.client.send_message(event.chat_id, msg) 
     
-@Drone.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/batch'))
+@katandika2022.on(events.NewMessage(incoming=True, from_users=AUTH, pattern='/batch'))
 async def _batch(event):
     if not event.is_private:
         return
@@ -42,8 +41,8 @@ async def _batch(event):
         await event.reply(r)
         return       
     if f'{event.sender_id}' in batch:
-        return await event.reply("You've already started one batch, wait for it to complete")
-    async with Drone.conversation(event.chat_id) as conv: 
+        return await event.reply("You've already started one batch, wait for it to complete you dumbfuck owner!")
+    async with katandika2022.conversation(event.chat_id) as conv: 
         if s != True:
             await conv.send_message("Send me the message link you want to start saving from, as a reply to this message.", buttons=Button.force_reply())
             try:
@@ -63,28 +62,22 @@ async def _batch(event):
                 return await conv.send_message("Cannot wait more longer for your response!")
             try:
                 value = int(_range.text)
-                if value > 100:
-                    return await conv.send_message("You can only get upto 100 files in a single batch.")
+                if value > 200:
+                    return await conv.send_message("You can only get upto 200 files in a single batch.")
             except ValueError:
                 return await conv.send_message("Range must be an integer!")
+            s, r = await check(userbot, Bot, _link)
             if s != True:
-                await conv.send_message("Batch Command Started")
+                await conv.send_message(r)
                 return
             batch.append(f'{event.sender_id}')
-            batch_.append(f'{event.sender_id}')
-            cd = await conv.send_message("**Batch process ongoing.**\n\nProcess completed: ", 
-                                    buttons=[[Button.inline("CANCEL❌", data="cancel")]])
-            await run_batch(userbot, Bot, event.sender_id, value, cd, _link) 
+            await run_batch(userbot, Bot, event.sender_id, _link, value) 
             conv.cancel()
-            batch.clear()
-            batch_.clear()
+            batch.pop(0)
             
-@Drone.on(events.callbackquery.CallbackQuery(data="cancel"))
-async def cancel(event):
-    batch_.clear()
-    
-async def run_batch(userbot, client, sender, range_, countdown, link):
-    for i in range(range_ + 1):
+            
+async def run_batch(userbot, client, sender, link, _range):
+    for i in range(_range):
         timer = 60
         if i < 25:
             timer = 5
@@ -92,35 +85,20 @@ async def run_batch(userbot, client, sender, range_, countdown, link):
             timer = 10
         if i < 100 and i > 50:
             timer = 15
+        if i < 150 and i > 100:
+            timer = 20
+        if i < 200 and i > 150:
+            timer = 25
         if not 't.me/c/' in link:
             if i < 25:
                 timer = 2
             else:
                 timer = 3
-        try: 
-            check_ = batch_[0]
-            count_down = f"**Batch process ongoing.**\n\nProcess completed: {i+1}"
-            out = await get_bulk_msg(userbot, client, sender, link, i) 
-            if not out == None:
-                if out - 5 > 300:
-                    await client.send_message(sender, f'You have floodwaits of {out - 5} seconds, cancelling batch') 
-                    batch_.clear()
-                    break
-                else:
-                    fw_alert = await client.send_message(sender, f'Sleeping for {out} second(s) due to telegram flooodwait.')
-                    await asyncio.sleep(out)
-                    await fw_alert.delete()
-                    await get_bulk_msg(userbot, client, sender, link, i) 
-            protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
-            await countdown.edit(count_down)
-            await asyncio.sleep(timer)
-            await protection.delete()
-        except IndexError:
-            await client.send_message(sender, "Batch successfully completed!")
-            await countdown.delete()
-            break
-        except Exception as e:
-            print(e)
-            if not countdown.text == count_down:
-                await countdown.edit(count_down)
-                
+        try:
+            await get_bulk_msg(userbot, client, sender, link, i) 
+        except FloodWait as fw:
+            await asyncio.sleep(fw.seconds + 5)
+            await get_bulk_msg(userbot, client, sender, link, i)
+        protection = await client.send_message(sender, f"Sleeping for `{timer}` seconds to avoid Floodwaits and Protect account!")
+        time.sleep(timer)
+        await protection.delete()
